20552e5949bc1846a7253c883442d5e795c83bc3
Marcel Iskrač <40259973+iskraM@users.noreply.github.com>
2022-04-21 09:01:46 +0200

    Changed build settings
    All dlls are now embed resources in .exe

---
+++ Code128Generator/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace Code128Generator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbCodes = new System.Windows.Forms.PictureBox();
+            this.rtbRawCodes = new System.Windows.Forms.RichTextBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCodes)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pbCodes
+            // 
+            this.pbCodes.BackColor = System.Drawing.Color.White;
+            this.pbCodes.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pbCodes.Location = new System.Drawing.Point(0, 0);
+            this.pbCodes.Name = "pbCodes";
+            this.pbCodes.Size = new System.Drawing.Size(440, 579);
+            this.pbCodes.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pbCodes.TabIndex = 2;
+            this.pbCodes.TabStop = false;
+            // 
+            // rtbRawCodes
+            // 
+            this.rtbRawCodes.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rtbRawCodes.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.rtbRawCodes.Location = new System.Drawing.Point(0, 0);
+            this.rtbRawCodes.Name = "rtbRawCodes";
+            this.rtbRawCodes.Size = new System.Drawing.Size(315, 579);
+            this.rtbRawCodes.TabIndex = 3;
+            this.rtbRawCodes.Text = "";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(10, 10);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.rtbRawCodes);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.AutoScroll = true;
+            this.splitContainer1.Panel2.Controls.Add(this.pbCodes);
+            this.splitContainer1.Size = new System.Drawing.Size(759, 579);
+            this.splitContainer1.SplitterDistance = 315;
+            this.splitContainer1.TabIndex = 4;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnGenerate.Location = new System.Drawing.Point(10, 547);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(759, 42);
+            this.btnGenerate.TabIndex = 3;
+            this.btnGenerate.Text = "Generiraj";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(779, 599);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(10);
+            this.Text = "Code 128 generator";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.pbCodes)).EndInit();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pbCodes;
+        private System.Windows.Forms.RichTextBox rtbRawCodes;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button btnGenerate;
+    }
+}
+
---
+++ Code128Generator/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZXing;
+
+namespace Code128Generator
+{
+    public partial class Form1 : Form
+    {
+        bool tabPressed = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            rtbRawCodes.Focus();
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            Bitmap codes = new Bitmap(pbCodes.Width, 10000);
+            Graphics graphics = Graphics.FromImage(codes);
+            BarcodeWriter writer = new BarcodeWriter() { Format = BarcodeFormat.CODE_128 };
+            writer.Options.Height = 70;
+
+            int y = 10;
+            int last = 2;
+            foreach (string s in rtbRawCodes.Lines)
+            {
+                if (!String.IsNullOrWhiteSpace(s.Trim()))
+                {
+                    if (s.StartsWith("/"))
+                    {
+                        graphics.SmoothingMode = SmoothingMode.AntiAlias;
+                        graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                        graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+                        graphics.DrawString(s.Replace('/', ' '), new Font("Tahoma", 14), Brushes.Black, new Point(codes.Width / 3, y));
+
+                        graphics.Flush();
+                        y += 30;
+                    }
+                    else
+                    {
+                        Bitmap code = writer.Write(s.Trim());
+                        graphics.DrawImage(code, codes.Width / 2 - code.Width / 2, y);
+                        y += code.Height + 30;
+                        last = code.Height;
+                    }
+                } else
+                {
+                    y += last / 2;
+                }
+            }
+
+            pbCodes.Image = codes;
+        }
+
+        protected override bool ProcessDialogKey(Keys keyData)
+        {
+            if (keyData == Keys.Tab) btnGenerate_Click(null, null);
+            return false;
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Prevent textbox beeping
+            if (e.KeyChar == '\t') e.Handled = true;
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            btnGenerate_Click(null, null);
+        }
+    }
+}
